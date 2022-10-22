eb2cc39a28b51c03cfe3a9e14a31519fb21bb7b0
My GIT <charvibasappa98@gmail.com>
2022-10-21 19:01:12 -0500

    USer Model

---
+++ houseassistance/src/main/java/com/housebooking/app/model/UserModel.java
@@ -0,0 +1,88 @@
+package com.housebooking.app.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "users", uniqueConstraints={@UniqueConstraint(columnNames = {"email" , "mobileNo"})})
+public class UserModel {
+	
+	public UserModel() {
+		super();
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String mobileNo;
+	private String password;
+	private String usertype;
+	private String securityQuestion;
+	private String securityAnswer;
+
+	public String getSecurityQuestion() {
+		return securityQuestion;
+	}
+	public void setSecurityQuestion(String securityQuestion) {
+		this.securityQuestion = securityQuestion;
+	}
+	public String getSecurityAnswer() {
+		return securityAnswer;
+	}
+	public void setSecurityAnswer(String securityAnswer) {
+		this.securityAnswer = securityAnswer;
+	}
+	public String getUsertype() {
+		return usertype;
+	}
+	public void setUsertype(String usertype) {
+		this.usertype = usertype;
+	}
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String toString(){ 
+		  return firstname+" "+lastname+" "+email+" "+mobileNo+" "+usertype+" "+securityQuestion+" "+securityAnswer;  
+		 }  
+
+}
+
