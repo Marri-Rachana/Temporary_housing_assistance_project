3ec6fd9255f5dfb680f6d91537ea35d1817ec4ea
yasoda-krishna63 <yasodakri.annapureddy@gmail.com>
2022-12-02 16:26:50 -0600

    Updated Recover Username

---
+++ houseassistance/src/main/java/com/housebooking/app/communication/Email.java
@@ -0,0 +1,29 @@
+package com.housebooking.app.communication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+public class Email implements Communication {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+
+    public int sendSimpleMail(SimpleMailMessage mailMessage)
+    {
+
+        // Try block to check for exceptions
+        try {
+
+            javaMailSender.send(mailMessage);
+            return 1;
+        }
+
+        // Catch block to handle the exceptions
+        catch (Exception e) {
+            return 0;
+        }
+    }
+}
